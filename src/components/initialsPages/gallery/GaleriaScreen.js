42a0257c5b645a445f13ba1ae5d23373b9a74531
darday <darday1980@gmail.com>
2023-04-03 11:14:05 -0500

    Gallery Finished

--- src/components/initialsPages/gallery/GaleriaScreen.js
+++ src/components/initialsPages/gallery/GaleriaScreen.js
@@ -4,76 +4,11 @@ import './Gallery.css'
 
 // import { FaWindowClose } from "react-icons/fa";
 import FaWindowClose from '@mui/icons-material/Close';
-import img1 from './img/img1.jpeg';
-import img2 from './img/img2.jpeg';
-import img3 from './img/img3.jpeg';
-import img4 from './img/img4.jpeg';
-import img5 from './img/img5.jpeg';
-import img6 from './img/img6.jpeg';
-import img7 from './img/img7.jpeg';
-import img8 from './img/img8.jpeg';
-import img9 from './img/img9.jpeg';
-import img10 from './img/img10.jpeg';
-import img11 from './img/img11.jpeg';
-import img12 from './img/img12.jpeg';
-import img13 from './img/img13.jpeg';
+
 import axios from 'axios';
 import { ApiStorage, ApiUrl } from '../../../services/ApiRest';
 
 export const GaleriaScreen = () => {
-    // let data = [
-    //     {
-    //         id: 1,
-    //         imgSrc: img1,
-    //     },
-    //     {
-    //         id: 2,
-    //         imgSrc: img2,
-    //     },
-    //     {
-    //         id: 3,
-    //         imgSrc: img3,
-    //     },
-    //     {
-    //         id: 4,
-    //         imgSrc: img4,
-    //     },
-    //     {
-    //         id: 5,
-    //         imgSrc: img5,
-    //     },
-    //     {
-    //         id: 6,
-    //         imgSrc: img6,
-    //     },
-    //     {
-    //         id: 7,
-    //         imgSrc: img7,
-    //     },
-    //     {
-    //         id: 8,
-    //         imgSrc: img8,
-    //     },
-    //     {
-    //         id: 9,
-    //         imgSrc: img9,
-    //     }, {
-    //         id: 10,
-    //         imgSrc: img10,
-    //     },
-    //     {
-    //         id: 11,
-    //         imgSrc: img11,
-    //     },
-    //     {
-    //         id: 12,
-    //         imgSrc: img12,
-    //     },
-    //     {
-    //         id: 13,
-    //         imgSrc: img13,
-    //     },
-    // ]
 
     const [data, setdata] = useState([]);
     const [isLoading, setisLoading] = useState(true);
